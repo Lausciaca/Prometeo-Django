595517c546a671f8ed377d15d850dcdbce394d00
Laureano <laureanosciacaluga@gmail.com>
2024-10-12 17:15:13 -0300

    Migrar modelo libro

---
+++ prometeo/libros/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Libro(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Titulo')
+    publisher = models.CharField(max_length=100, verbose_name='Editorial')
+    # materia = models.CharField(max_length=100, verbose_name='Editorial')
+    stock = models.IntegerField(verbose_name='Cantidad')
+    author = models.CharField(max_length=100, verbose_name='Autor')
+    isbn = models.IntegerField(verbose_name='ISBN')  
+    created = models.DateTimeField(auto_now_add=True)  
+    updated = models.DateTimeField(auto_now=True)  
+    creator_id = models.ForeignKey(User, verbose_name="Creador", on_delete=models.CASCADE)
