34626a31dc4d739a79b0febd9ee056cd6460ee5e
Laureano <laureanosciacaluga@gmail.com>
2024-10-12 18:49:21 -0300

    configurar urls core

--- prometeo/core/urls.py
+++ prometeo/core/urls.py
@@ -3,4 +3,10 @@ from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
+    path('institucion/', HomePageView.as_view(), name="institucion"),
+    path('alumnos/centro-de-estudiantes', HomePageView.as_view(), name="alumnos_centro-de-estudiantes"),
+    path('alumnos/inscripciones', HomePageView.as_view(), name="alumnos_inscripciones"),
+    path('alumnos/examenes', HomePageView.as_view(), name="alumnos_examenes"),
+    path('alumnos/horarios', HomePageView.as_view(), name="alumnos_horarios"),
+    path('alumnos/modalidades', HomePageView.as_view(), name="alumnos_horarios"),
 ]
