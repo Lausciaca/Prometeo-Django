8286d2278c87472d24a52bdde62b017e8fc92d64
Laureano <laureanosciacaluga@gmail.com>
2024-10-13 08:47:24 -0300

    Arreglar problema nav

--- prometeo/core/urls.py
+++ prometeo/core/urls.py
@@ -3,13 +3,13 @@ from .views import HomePageView, InstitucionPageView, SecretariaPageView, Contac
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
-    path('institucion/', InstitucionPageView.as_view(), name="institucion"),
+    path('institucion', InstitucionPageView.as_view(), name="institucion"),
     path('alumnos/centro-de-estudiantes', HomePageView.as_view(), name="alumnos_centro-de-estudiantes"),
     path('alumnos/inscripciones', HomePageView.as_view(), name="alumnos_inscripciones"),
     path('alumnos/examenes', HomePageView.as_view(), name="alumnos_examenes"),
     path('alumnos/horarios', HomePageView.as_view(), name="alumnos_horarios"),
     path('alumnos/modalidades/', include('modalidades.urls')),
     path('biblioteca', include('libros.urls')),
-    path('secretaria/', SecretariaPageView.as_view(), name='secretaria'),
-    path('contacto/', ContactoPageView.as_view(), name='contacto')
+    path('secretaria', SecretariaPageView.as_view(), name='secretaria'),
+    path('contacto', ContactoPageView.as_view(), name='contacto')
 ]
