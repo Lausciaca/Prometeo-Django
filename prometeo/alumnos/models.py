a3e0a89b382f40fb5f07773752a69be27bc1d256
Laureano <laureanosciacaluga@gmail.com>
2024-10-13 09:25:59 -0300

    Añadir modelos de app alumnos

--- prometeo/alumnos/models.py
+++ prometeo/alumnos/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Curso(models.Model):
+    name = models.IntegerField(verbose_name='Curso')
+    created = models.DateTimeField(auto_now_add=True)  
+    updated = models.DateTimeField(auto_now=True)
+    creator_id = models.ForeignKey(User, verbose_name="Creador", on_delete=models.CASCADE)
+
+    def __str__(self):  
+        return self.name
+    
+class Division(models.Model):
+    name = models.CharField(max_length=50)
+    curso = models.ForeignKey(Curso, verbose_name='Curso', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)  
+    updated = models.DateTimeField(auto_now=True)  
+    creator_id = models.ForeignKey(User, verbose_name="Creador", on_delete=models.CASCADE)
+
+    def __str__(self):  
+        return (self.name + self.curso)
+
+class CDE(models.Model):
+    content= models.TextField(verbose_name='Contenido')
+    created = models.DateTimeField(auto_now_add=True)  
+    updated = models.DateTimeField(auto_now=True)  
+    creator_id = models.ForeignKey(User, verbose_name="Creador", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.created
+    
+class Inscripcion(models.Model):
+    title = models.CharField(verbose_name='Titulo', max_length=100)
+    content = models.TextField(verbose_name='Contenido')
+    link = models.URLField(verbose_name='Link a formulario', max_length=200)
+    created = models.DateTimeField(auto_now_add=True)  
+    updated = models.DateTimeField(auto_now=True)  
+    creator_id = models.ForeignKey(User, verbose_name="Creador", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+class Examen(models.Model):
+    title = models.CharField(verbose_name='Titulo', max_length=100)
+    inscription_start_date = models.DateField(auto_now=False, auto_now_add=False, verbose_name='Fecha de inicio de inscripcion')
+    inscription_end_date = models.DateField(auto_now=False, auto_now_add=False, verbose_name='Fecha de fin de inscripcion')
+    test_start_date = models.DateField(auto_now=False, auto_now_add=False, verbose_name='Fecha de inicio de examenes')
+    test_end_date = models.DateField(auto_now=False, auto_now_add=False, verbose_name='Fecha de fin de examenes')
+    content = models.TextField(verbose_name='Contenido')
+    link = models.URLField(verbose_name='Link formulario', max_length=200)
+    created = models.DateTimeField(auto_now_add=True)  
+    updated = models.DateTimeField(auto_now=True)  
+    creator_id = models.ForeignKey(User, verbose_name="Creador", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+class Horario(models.Model):
+    division = models.ForeignKey(Division, verbose_name='Curso/Division', on_delete=models.CASCADE)
+    link = models.URLField(verbose_name='Link a horario', max_length=200)
+    
+    
--- prometeo/alumnos/views.py
+++ prometeo/alumnos/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from .models import CDE, Inscripcion, Examen, Horario
 
 
 # Create your views here.
 class CDEPageView(ListView):
+    model = CDE
     template_name = 'alumnos/centro_de_estudiantes.html'
     
 class InscripcionesPageView(ListView):
+    model = Inscripcion
     template_name = 'alumnos/inscripciones.html'
     
 class ExamenesPageView(ListView):
+    model = Examen
     template_name = 'alumnos/examenes.html'
     
 class HorariosPageView(ListView):
+    model = Horario
     template_name = 'alumnos/horarios.html'
     
