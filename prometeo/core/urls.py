5799f6009a4309289382dc5fb50bfa85af575612
Laureano <laureanosciacaluga@gmail.com>
2024-10-13 07:59:23 -0300

    Añadir pagina institucion

--- prometeo/core/urls.py
+++ prometeo/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from .views import HomePageView
+from .views import HomePageView, InstitucionPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
-    path('institucion/', HomePageView.as_view(), name="institucion"),
+    path('institucion/', InstitucionPageView.as_view(), name="institucion"),
     path('alumnos/centro-de-estudiantes', HomePageView.as_view(), name="alumnos_centro-de-estudiantes"),
     path('alumnos/inscripciones', HomePageView.as_view(), name="alumnos_inscripciones"),
     path('alumnos/examenes', HomePageView.as_view(), name="alumnos_examenes"),
--- prometeo/core/views.py
+++ prometeo/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.base import TemplateView
 from novedades.models import Novedad
 
 # Create your views here.
@@ -7,3 +8,5 @@ class HomePageView(ListView):
     model = Novedad
     template_name = 'core/home.html'
     
+class InstitucionPageView(TemplateView):
+    template_name = 'core/institucion.html'
